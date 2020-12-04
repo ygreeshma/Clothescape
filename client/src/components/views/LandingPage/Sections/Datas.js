21e2583dd409a52b11dca75c612675f0575978cc
ygreeshma <y.greeshmareddy@gmail.com>
2020-12-04 19:56:56 +0530

    Initial commit

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,71 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $199",
+        "array": [0, 199]
+    },
+    {
+        "_id": 2,
+        "name": "$200 to $249",
+        "array": [200, 249]
+    },
+    {
+        "_id": 3,
+        "name": "$250 to $279",
+        "array": [250, 279]
+    },
+    {
+        "_id": 4,
+        "name": "$280 to $299",
+        "array": [280, 299]
+    },
+    {
+        "_id": 5,
+        "name": "More than $300",
+        "array": [300, 1500000]
+    }
+]
+
+
+
+const categories = [
+    {
+        "_id": 1,
+        "name": "Women's Tops"
+    },
+    {
+        "_id": 2,
+        "name": "Mens's tops"
+    },
+    {
+        "_id": 3,
+        "name": "Skirts"
+    },
+    {
+        "_id": 4,
+        "name": "Women's pants"
+    },
+    {
+        "_id": 5,
+        "name": "Men's pants"
+    },
+    {
+        "_id": 6,
+        "name": "Kids"
+    },
+    {
+        "_id": 7,
+        "name": "Shorts"
+    }
+]
+
+export {
+    price,
+    categories
+}
---
+++ server/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:
+      "mongodb+srv://user_abc:user_12345@cluster0.hr5gn.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  };
+  
